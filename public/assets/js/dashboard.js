1242322813e3c89ba1a35863c1797e63b59103c1
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-07-21 15:07:05 +0200

    Update dashboard.js

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -16,20 +16,6 @@ document.addEventListener('DOMContentLoaded', function() {
     })
 });
 
-const copy = str => {
-    const el = document.createElement('textarea');
-    
-    el.value = str;
-    el.setAttribute('readonly', '');
-    el.style.position = 'absolute';
-    el.style.left = '-9999px';
-    document.body.appendChild(el);
-
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-};
-
 const createLinkForm = document.querySelector('form#linkCreate');
 createLinkForm.addEventListener('submit', e => {
     e.preventDefault();
